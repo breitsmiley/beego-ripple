07131f4518324b0a81ccf3c0f72253c92c885056
Pavel Tsarenko <breitsmiley@gmail.com>
2018-01-08 13:11:51 +0200

    added fail route - fix

--- controllers/default.go
+++ controllers/default.go
@@ -31,19 +31,6 @@ func (c *MainController) Index() {
 
 }
 
-// @router /dev/fail [get]
-func (c *MainController) DevFail() {
-
-	data, err := models.GetQuizViewData()
-
-	if err != nil {
-		beego.Warn(err)
-		c.Abort("503")
-	}
-	c.Data["data"] = &data
-	c.TplName = "fail.html"
-}
-
 // @router /init/hde15knQw [get]
 func (c *MainController) InitDb() {
 
